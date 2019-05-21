02464b8bc38de368fb92c2a6e7cd9f0808bf089e
Elvis <elvistavares@outlook.com>
2019-05-20 23:54:32 -0300

    V2.5 - estilizaçao bootsrap

--- index.php
+++ index.php
@@ -12,19 +12,30 @@ if(isset($_SESSION['msg'])){
         <meta charset="utf-8">
         <title>Editor de Texto</title>
         <script src="ckeditor.js"></script>
+
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
     </head>
     <body>
         
         <form method="POST" action="processa.php">
-            <textarea name="exemplo" id="editor1" rows="10" cols="80">
+           <div class="form-group">
+                <label for="exampleFormControlTextarea1">Caixa de texto</label>
+                <textarea class="form-control" name="exemplo" id="editor1" rows="10" cols="80">
                
-            </textarea>
-            <script>
-              
-                CKEDITOR.replace( 'editor1' );
-            </script>
-
-            <input type="submit" value="Postar" name="postar">
+               </textarea>
+               <script>
+                 
+                   CKEDITOR.replace( 'editor1' );
+               </script>
+   
+               <input type="submit" value="Postar" name="postar">
+           </div>
         </form>
     </body>
 </html>
--- lista.php
+++ lista.php
@@ -1,14 +1,21 @@
 <?php
 include_once("conexao.php");
+include_once "cab.php";
 
 $result_sql = "SELECT * FROM texto";
 $resultado_sql = mysqli_query($conn, $result_sql);
 
-while($row = mysqli_fetch_assoc($resultado_sql)){
-    echo $row['texto_c']."<br><hr>";
-}
+
 
 ?>
 
 
+<ul class="list-group">
+<?php
+        while($row = mysqli_fetch_assoc($resultado_sql)){
+            echo "<li class='list-group-item'>".$row['texto_c']."</li>";
+        }
+        ?>
 
+  
+</ul>
